7763791b0464dab9a559681d9344b9c60279cf14
jose.quero <jose.quero@workhuman.com>
2020-08-28 14:50:16 +0100

    refactoring

--- src/main/java/salad/Salad.java
+++ src/main/java/salad/Salad.java
@@ -2,17 +2,41 @@ package salad;
 
 import ingredient.Vegetable;
 
-import java.util.*;
+import javax.xml.bind.ValidationEvent;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
-public class Salad extends SaladTemplate {
+public abstract class SaladTemplate {
 
+    Set<Vegetable> ingredients;
+    int saladCalories = 0;
 
-    public Salad(List<Vegetable> ingredients, int calories) {
-        super.ingredients = ingredients;
-        super.saladCalories = calories;
+    public SaladTemplate (Set<Vegetable> ingredients, int calories){
+        this.ingredients = ingredients;
+        this.saladCalories = calories;
     }
 
-    public Salad() {
+    public SaladTemplate() {
     }
 
+    public int getCalories() {;
+        return saladCalories;
+    }
+
+    public Set<Vegetable> getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Your salad is ready. Ingredients: ";
+        for(Vegetable eachIngredient:ingredients) {
+            result = result.concat(eachIngredient.toString());
+            result = result.concat(", ");
+        }
+        result = result.substring(0, result.length() - 2);
+         return result + ". Total Calories = " + saladCalories;
+    }
 }
